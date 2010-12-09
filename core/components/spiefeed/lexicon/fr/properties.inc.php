1075d76fc1288570ecd468a168954cb060fc16b0
Romain <romain@meltingmedia.net>
2010-12-09 09:42:57 +0100

    french translation of properties

---
+++ core/components/spiefeed/lexicon/fr/properties.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     spieFeed
+ * @subpackage  lexicon
+ */
+
+$_lang['spiefeed.tpl'] = 'Chunk de modèle par défaut';
+$_lang['spiefeed.tplPath'] = 'Chemin du modèle par défaut';
+$_lang['spiefeed.tplFile'] = 'Nom du fichier de modèle par défaut';
+$_lang['spiefeed.css'] = 'Fichier CSS par défaut';
+$_lang['spiefeed.firstRowCls'] = 'Classe CSS par défaut pour la première « rangée »';
+$_lang['spiefeed.lastRowCls'] = 'Classe CSS par défaut pour la dernière « rangée »';
+$_lang['spiefeed.rowCls'] = 'Classe CSS par défaut pour chaque « rangée »';
+$_lang['spiefeed.oddRowCls'] = 'Classe CSS par défaut pour les « rangées » impaires';
